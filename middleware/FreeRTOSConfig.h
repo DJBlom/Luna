d879047dc5b8adbc648e88b216c1601177f4748f
DJBlom <63865918+DJBlom@users.noreply.github.com>
2023-04-10 08:37:29 -0400

    Update FreeRTOSConfig.h

--- middleware/FreeRTOSConfig.h
+++ middleware/FreeRTOSConfig.h
@@ -1,10 +1,6 @@
 /*
- * FreeRTOS Kernel V10.4.3 LTS Patch 2
+ * FreeRTOS Kernel V10.4.3
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- * Copyright (C) 2019-2021 Cypress Semiconductor Corporation, or a subsidiary of
- * Cypress Semiconductor Corporation.  All Rights Reserved.
- *
- * Updated configuration to support CM33.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -25,7 +21,6 @@
  *
  * https://www.FreeRTOS.org
  * https://github.com/FreeRTOS
- * http://www.cypress.com
  *
  */
 
@@ -44,41 +39,35 @@
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
-/* Ensure stdint is only used by the compiler, and not the assembler. */
-#if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
-	#include <stdint.h>
-	extern uint32_t SystemCoreClock;
-#endif
-
 #define configUSE_PREEMPTION                    1
-#define configCPU_CLOCK_HZ                      SystemCoreClock
-#define configTICK_RATE_HZ                      ((TickType_t ) 200)
+#define configUSE_TICKLESS_IDLE                 0
+#define configCPU_CLOCK_HZ                      (SystemCoreClock)
+#define configTICK_RATE_HZ                      ((TickType_t)1000)
 #define configMAX_PRIORITIES                    7
-#define configMINIMAL_STACK_SIZE                128
-#define configMAX_TASK_NAME_LEN                 16
+#define configMINIMAL_STACK_SIZE                ((unsigned short)90)
+#define configMAX_TASK_NAME_LEN                 20
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
 #define configUSE_TASK_NOTIFICATIONS            1
 #define configUSE_MUTEXES                       1
 #define configUSE_RECURSIVE_MUTEXES             1
 #define configUSE_COUNTING_SEMAPHORES           0
-#define configUSE_BINARY_SEMAPHORES             1
-#define configQUEUE_REGISTRY_SIZE               10
+#define configQUEUE_REGISTRY_SIZE               8
 #define configUSE_QUEUE_SETS                    0
-#define configUSE_TIME_SLICING                  0
+#define configUSE_TIME_SLICING                  1
+#define configUSE_NEWLIB_REENTRANT              0
 #define configENABLE_BACKWARD_COMPATIBILITY     0
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
 
-/* Compile-time macros to enable or disable TrustZone, Memory Protection Unit (MPU) and Floating Point Unit (FPU) support. */
-#define configENABLE_FPU                        0
-#define configENABLE_MPU                        0
-#define configENABLE_TRUSTZONE                  0
-#define configRUN_FREERTOS_SECURE_ONLY          0
+/* Used memory allocation (heap_x.c) */
+#define configFRTOS_MEMORY_SCHEME               4
+/* Tasks.c additions (e.g. Thread Aware Debug capability) */
+#define configINCLUDE_FREERTOS_TASK_C_ADDITIONS_H 1
 
 /* Memory allocation related definitions. */
 #define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   ((size_t )(20*1024))
+#define configTOTAL_HEAP_SIZE                   ((size_t)(10240))
 #define configAPPLICATION_ALLOCATED_HEAP        0
 
 /* Hook function related definitions. */
@@ -93,38 +82,27 @@
 #define configUSE_TRACE_FACILITY                1
 #define configUSE_STATS_FORMATTING_FUNCTIONS    0
 
+/* Task aware debugging. */
+#define configRECORD_STACK_HIGH_ADDRESS         1
+
 /* Co-routine related definitions. */
 #define configUSE_CO_ROUTINES                   0
-#define configMAX_CO_ROUTINE_PRIORITIES         1
+#define configMAX_CO_ROUTINE_PRIORITIES         0
 
 /* Software timer related definitions. */
 #define configUSE_TIMERS                        1
-#define configTIMER_TASK_PRIORITY               3
+#define configTIMER_TASK_PRIORITY               (configMAX_PRIORITIES - 1)
 #define configTIMER_QUEUE_LENGTH                10
-#define configTIMER_TASK_STACK_DEPTH            ( configMINIMAL_STACK_SIZE * 2 )
+#define configTIMER_TASK_STACK_DEPTH            (configMINIMAL_STACK_SIZE * 2)
 
+/* Define to trap errors during development. */
+#define configASSERT(x) if(( x) == 0) {taskDISABLE_INTERRUPTS(); for (;;);}
 
-/*
-configMAX_SYSCALL_INTERRUPT_PRIORITY sets the highest interrupt priority from which
-interrupt safe FreeRTOS API functions can be called.
-!!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
-See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html
-*/
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY    0x3F
-
-/* configMAX_API_CALL_INTERRUPT_PRIORITY is a new name for configMAX_SYSCALL_INTERRUPT_PRIORITY
- that is used by newer ports only. The two are equivalent. */
-#define configMAX_API_CALL_INTERRUPT_PRIORITY   configMAX_SYSCALL_INTERRUPT_PRIORITY
-
-
-/* Set the following definitions to 1 to include the API function, or zero
-to exclude the API function. */
+/* Optional functions - most linkers will remove unused functions anyway. */
 #define INCLUDE_vTaskPrioritySet                1
 #define INCLUDE_uxTaskPriorityGet               1
 #define INCLUDE_vTaskDelete                     1
-#define INCLUDE_vTaskCleanUpResources           0
 #define INCLUDE_vTaskSuspend                    1
-#define INCLUDE_xResumeFromISR                  1
 #define INCLUDE_vTaskDelayUntil                 1
 #define INCLUDE_vTaskDelay                      1
 #define INCLUDE_xTaskGetSchedulerState          1
@@ -132,76 +110,48 @@ to exclude the API function. */
 #define INCLUDE_uxTaskGetStackHighWaterMark     0
 #define INCLUDE_xTaskGetIdleTaskHandle          0
 #define INCLUDE_eTaskGetState                   0
-#define INCLUDE_xEventGroupSetBitFromISR        1
 #define INCLUDE_xTimerPendFunctionCall          1
 #define INCLUDE_xTaskAbortDelay                 0
 #define INCLUDE_xTaskGetHandle                  0
 #define INCLUDE_xTaskResumeFromISR              1
 
-/* Normal assert() semantics without relying on the provision of an assert.h
-header file. */
-#if defined(NDEBUG)
-#define configASSERT( x ) CY_UNUSED_PARAMETER( x )
-#else
-#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }	
+
+
+#if defined(__ICCARM__)||defined(__CC_ARM)||defined(__GNUC__)
+    /* Clock manager provides in this variable system core clock frequency */
+    #include <stdint.h>
+    extern uint32_t SystemCoreClock;
 #endif
+
+/* Interrupt nesting behaviour configuration. Cortex-M specific. */
+#ifdef __NVIC_PRIO_BITS
+/* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
+#define configPRIO_BITS __NVIC_PRIO_BITS
+#else
+#define configPRIO_BITS 4 /* 15 priority levels */
+#endif
+
+/* The lowest interrupt priority that can be used in a call to a "set priority"
+function. */
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY ((1U << (configPRIO_BITS)) - 1)
+
+/* The highest interrupt priority that can be used by any interrupt service
+routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
+INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
+PRIORITY THAN THIS! (higher priorities are lower numeric values. */
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 2
+
+/* Interrupt priorities used by the kernel port layer itself.  These are generic
+to all Cortex-M ports, and do not rely on any particular library functions. */
+#define configKERNEL_INTERRUPT_PRIORITY (configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
+/* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
+See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
-standard names - or at least those used in the unmodified vector table. */
-#define vPortSVCHandler     SVC_Handler
-#define xPortPendSVHandler  PendSV_Handler
+standard names. */
+#define vPortSVCHandler SVC_Handler
+#define xPortPendSVHandler PendSV_Handler
 #define xPortSysTickHandler SysTick_Handler
-
-/* Dynamic Memory Allocation Schemes */
-#define HEAP_ALLOCATION_TYPE1                   (1)     /* heap_1.c*/
-#define HEAP_ALLOCATION_TYPE2                   (2)     /* heap_2.c*/
-#define HEAP_ALLOCATION_TYPE3                   (3)     /* heap_3.c*/
-#define HEAP_ALLOCATION_TYPE4                   (4)     /* heap_4.c*/
-#define HEAP_ALLOCATION_TYPE5                   (5)     /* heap_5.c*/
-#define NO_HEAP_ALLOCATION                      (0)
-
-#define configHEAP_ALLOCATION_SCHEME            (HEAP_ALLOCATION_TYPE3)
-
-/* Check if the ModusToolbox Device Configurator Power personality parameter
- * "System Idle Power Mode" is set to either "CPU Sleep" or "System Deep Sleep".
- */
-#if defined(CY_CFG_PWR_SYS_IDLE_MODE) && \
-    ((CY_CFG_PWR_SYS_IDLE_MODE == CY_CFG_PWR_MODE_SLEEP) || \
-    (CY_CFG_PWR_SYS_IDLE_MODE == CY_CFG_PWR_MODE_DEEPSLEEP) || \
-    (CY_CFG_PWR_SYS_IDLE_MODE == CY_CFG_PWR_MODE_DEEPSLEEP_RAM))
-
-/* Enable low power tickless functionality. The RTOS abstraction library
- * provides the compatible implementation of the vApplicationSleep hook:
- * https://github.com/infineon/abstraction-rtos#freertos
- * The Low Power Assistant library provides additional portable configuration layer
- * for low-power features supported by the PSoC 6 devices:
- * https://github.com/infineon/lpa
- */
-extern void vApplicationSleep( uint32_t xExpectedIdleTime );
-#define portSUPPRESS_TICKS_AND_SLEEP( xIdleTime ) vApplicationSleep( xIdleTime )
-#define configUSE_TICKLESS_IDLE                 2
-
-#else
-#define configUSE_TICKLESS_IDLE                 0
-#endif
-
-/* Deep Sleep Latency Configuration */
-#if ( CY_CFG_PWR_DEEPSLEEP_LATENCY > 0 )
-#define configEXPECTED_IDLE_TIME_BEFORE_SLEEP   CY_CFG_PWR_DEEPSLEEP_LATENCY
-#endif
-
-/* Allocate newlib reeentrancy structures for each RTOS task.
- * The system behavior is toolchain-specific.
- *
- * GCC toolchain: the application must provide the implementation for the required
- * newlib hook functions: __malloc_lock, __malloc_unlock, __env_lock, __env_unlock.
- * FreeRTOS-compatible implementation is provided by the clib-support library:
- * https://github.com/infineon/clib-support
- *
- * ARM/IAR toolchains: the application must provide the reent.h header to adapt
- * FreeRTOS's configUSE_NEWLIB_REENTRANT to work with the toolchain-specific C library.
- * The compatible implementations are also provided by the clib-support library.
- */
-#define configUSE_NEWLIB_REENTRANT              0
 
 #endif /* FREERTOS_CONFIG_H */
